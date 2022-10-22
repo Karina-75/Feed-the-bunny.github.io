09dd5a6be7e578fad248e311aa505fd01a204183
Karina-75 <110104516+Karina-75@users.noreply.github.com>
2022-10-22 14:13:30 +0300

    Add files via upload

--- link.js
+++ link.js
@@ -1,16 +1,20 @@
-class Link {
-    constructor(bodyA, bodyB) {
-        var lastLink = bodyA.body.bodies.length - 2
-        this.link = Constraint.create({
-            bodyA:bodyA.body.bodies[lastLink],
-            bodyB:bodyB,
-            pointA:{x:0,y:0},
-            pointB:{x:0,y:0},
-            stiffness:0.01,
-            length:-10
-
+class Link{
+    constructor(bodyA,bodyB)
+    {
+      var lastlink = bodyA.body.bodies.length-2;
+     this.link = Constraint.create(
+        {
+          bodyA:bodyA.body.bodies[lastlink],
+          pointA:{x:0,y:0},
+          bodyB:bodyB,
+          pointB:{x:0,y:0},
+          length:-10,
+          stiffness:0.01
         });
-
-        World.add(world,this.link);       
-    }
+        World.add(engine.world,this.link);
+    } 
+  detache(){
+    World.remove(engine.world,this.link);
+  }
 }
+
--- sketch.js
+++ sketch.js
@@ -10,34 +10,71 @@ const Composite = Matter.Composite;
 let engine;
 let world;
 var ground;
-var rope;
-var fruit;
+var fruit,rope;
 var fruit_con;
 
-function setup() {
-  createCanvas(500, 700);
+var bg_img;
+var food;
+var rabbit;
+var bunny;
+var button
+
+
+function preload()
+{
+  bg_img = loadImage('background.png');
+  food = loadImage('melon.png');
+  rabbit = loadImage('Rabbit-01.png');
+}
+
+function setup() 
+{
+  createCanvas(500,700);
   frameRate(80);
   engine = Engine.create();
   world = engine.world;
-  ground = new Ground(200, 680, 600, 20);
-  rope = new Rope(6, { x: 245, y: 30 });
-  fruit = Bodies.circle(300, 300, 15);
-  Matter.Composite.add(rope.body, fruit);
-  fruit_con = new Link(rope, fruit);
+  ground = new Ground(200,680,600,20);
+
+  rope = new Rope(7,{x:245,y:30});
+  fruit = Bodies.circle(300,300,20);
+  Matter.Composite.add(rope.body,fruit);
+
+  fruit_con = new Link(rope,fruit);
+
   rectMode(CENTER);
   ellipseMode(RADIUS);
   textSize(50)
+  imageMode(CENTER);
+  bunny = createSprite(250, 600, 100, 100);
+  bunny.addImage(rabbit)
+  bunny.scale = 0.3
 
+  button = createImg("cut_button.png");
+  button.position(220, 30);
+  button.size(50,50);
+  button.mouseClicked(drop)
+   
 }
 
-function draw() {
+function draw() 
+{
   background(51);
-  ground.show();
+
+  image(bg_img,width/2,height/2,490,690);
+
+  image(food,fruit.position.x,fruit.position.y,70,70);
   rope.show();
-  ellipse(fruit.position.x, fruit.position.y, 15, 15);
   Engine.update(engine);
+  ground.show();
+
+  drawSprites();
 
 
-
-
+ 
+   
+}
+function drop(){
+  console.log("buttonhasbeenclicked")
+  rope.break()
+  fruit_con.detache()
 }
